46af076d8b038addfe1055afa71e5e13e6cf369c
ismartashish <141064255+ismartashish@users.noreply.github.com>
2025-12-29 10:53:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/New-Portfolio-Frontend">
+    <BrowserRouter>
 
       <ThemeProvider>
         <AuthProvider>
